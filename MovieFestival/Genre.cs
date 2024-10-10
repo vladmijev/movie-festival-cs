c37586009b36010be51d8a977718f39a09013e0e
Vladimir Jevtovic <vladimir.jevtovic@traceone.com>
2024-10-10 11:17:26 +0200

    fix: Create Genre class

---
+++ MovieFestival/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFestival
+{
+	public class Genre
+	{
+        public string Name { get; set; }
+
+        public Genre(string name)
+        {
+            this.Name = name;   
+        }
+
+        public string GetData()
+        {
+            return $"{Name[0]}{Name[Name.Length-1]}".ToUpper(); 
+        }
+
+	}
+}
